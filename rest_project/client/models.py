7b202683f4513d03f93cfe771fd2051fb8958600
Dmitry Lvovich Grachev <grdmitriyl@gmail.com>
2023-03-21 04:15:25 +0300

    new: getting coordinated for city
    	- get latitude and longitude for city

--- rest_project/client/models.py
+++ rest_project/client/models.py
@@ -3,13 +3,13 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django_resized import ResizedImageField
 
-def client_dir_path(instance, filename):
-    """file will be uploaded to MEDIA_ROOT /client_<id>/<filename>"""
-    return f'uploads/'
-    # {instance.user.id}    {filename}
-
 
 class CustomUser(AbstractUser):
+    """
+    Model of client-customuser DB table.
+
+    This model needs for custom authentication.
+    """
     username = models.CharField(max_length=64, blank=True, null=True)
     email = models.EmailField('email address', unique=True)
 
@@ -21,7 +21,12 @@ class CustomUser(AbstractUser):
 
 
 class Client(models.Model):
-    """Client db"""
+    """
+    Model of client-client DB table.
+
+    user_id (user class attribute) field here
+    is pk of client-customuser DB table.
+    """
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
                                 on_delete=models.CASCADE,
                                 related_name='profile')
@@ -37,20 +42,8 @@ class Client(models.Model):
         blank='NULL',
         )
     birth_date = models.DateField(blank=True)
-
     photo = ResizedImageField(
         upload_to='uploads/',
         blank=True,
         size=[600, 600],
     )
-
-    # photo = models.ImageField(
-    #     upload_to='uploads/',
-    #     blank=True,
-    # )
-
-
-
-# class Photo(models.Model):
-#     '''Separate db for photos'''
-#     photo = models.ImageField(upload_to=client_dir_path)
--- rest_project/weather_api/views.py
+++ rest_project/weather_api/views.py
@@ -1,9 +1,38 @@
+from pathlib import Path
+
 import requests
 from django.conf import settings
 from rest_framework.permissions import AllowAny
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+
+def get_coordinates(request: dict) -> dict:
+    """
+    replaces "city" from request dictionary by
+    appropriate "latitude" and "longitude"
+    """
+    if 'city' in request:
+        city = request['city']
+        with open(Path('weather_api/data/cities.csv'), newline='') as csv_file:
+            print(csv_file.readline())
+            while True:
+                csv_line = csv_file.readline()
+                if city.capitalize() in csv_line:
+                    _, latitude, longitude = csv_line.split(',')
+                    break
+            coodrinates = {
+                'latitude': latitude.strip(),
+                'longitude': longitude.strip(),
+            }
+            del request['city']
+            request = {**request, **coodrinates}
+            print(f"{request=}")
+        return request
+
+    else:
+        return request
 
 
 class WeatherAPIView(APIView):
@@ -13,17 +42,25 @@ class WeatherAPIView(APIView):
     def get(self, request):
         url = 'https://archive-api.open-meteo.com/v1/era5'
         payload = {
-            'latitude': 52.2,
-            'longitude': 13.2,
+            # 'latitude': 52.2,
+            # 'longitude': 13.2,
             'start_date': '2021-01-01',
             'end_date': '2021-01-01',
             'daily': 'temperature_2m_max',
             'timezone': settings.TIME_ZONE,
         }
+        payload = {**payload.copy(), **get_coordinates(request.data)}
 
-        payload = {**payload.copy(), **request.data}
+
 
         response = requests.get(url, params=payload)
+        print(f"{type(response)=}")
+        if 'error' in response.json():
+            return Response({'finite': 'a la camedie'})
+
+
+
         response = Response(response.json())
+
 
         return response
